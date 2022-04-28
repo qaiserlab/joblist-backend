9fb4dd8f9690792ebf2fcc688c64d2fce2f76dce
Fadlun Anaturdasa <f.anaturdasa@gmail.com>
2022-04-28 18:56:17 +0700

    reset

--- src/@expresso/constants/ConstAllowedOrigins.ts
+++
@@ -1,5 +0,0 @@
-const allowedOrigins = [
-  'http://localhost:3333', // Frontend Web
-]
-
-export default allowedOrigins
--- src/controllers/Account/Role/schema.ts
+++
@@ -1,9 +0,0 @@
-import * as yup from 'yup'
-
-const create = yup.object().shape({
-  name: yup.string().required('name is required'),
-})
-
-const roleSchema = { create }
-
-export default roleSchema
