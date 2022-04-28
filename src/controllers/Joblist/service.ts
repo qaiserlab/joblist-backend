8854c0d79be80c287a7510ccb50794d7978d1b53
Fadlun Anaturdasa <f.anaturdasa@gmail.com>
2022-04-28 21:45:24 +0700

    fix: paging total params

--- src/controllers/Joblist/service.ts
+++ src/controllers/Joblist/service.ts
@@ -7,13 +7,24 @@ class JoblistService {
    * @param req Request
    */
   public static async getAll(req: Request) {
+    const url = 'http://dev3.dansmultipro.co.id/api/recruitment/positions.json'
+    
+    const reqQuery = req.query
+
     const response = await axios.get(
-      'http://dev3.dansmultipro.co.id/api/recruitment/positions.json',
-      { params: req.query }
+      url,
+      { params: reqQuery }
+    )
+
+    delete reqQuery.page
+
+    const responseTotal = await axios.get(
+      url,
+      { params: reqQuery }
     )
 
     const data = response.data
-    const total = data.length
+    const total = responseTotal.data.length
 
     return { message: `${total} data has been received.`, data, total }
   }
